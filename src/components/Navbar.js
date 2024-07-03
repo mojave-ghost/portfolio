c1b52fbc1b2d9cde8fb5a2ab424ed6c5a03fb454
marioCoding <marosw.sal@gmail.com>
2024-07-03 15:29:28 -0700

    Refactored App.css file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function Navbar() {
+const Navbar = () => {
   return (
     <nav className="nav-bar">
       <h1>Mario Salinas</h1>
       <ul className="relative flex items-center justify-center
-                    text-white">
+                      text-white">
         <li><a href="#home">Home</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#skills">Skills</a></li>
