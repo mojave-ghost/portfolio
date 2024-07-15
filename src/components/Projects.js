46f14be8c171522436e0c679e346d46d131db761
marioCoding <marosw.sal@gmail.com>
2024-07-15 16:42:01 -0700

    Projects component is now responsive && project-img class now has a drop shadow

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,14 +6,16 @@ function Projects() {
   return (
     <section>
       <h2 className="m-4 text-2xl text-primary font-montserrat" id="projects">Projects</h2>
-      <div className="grid grid-cols-1 mx-8">
+      <div className="grid grid-cols-1 mx-8
+                      lg:grid-cols-2 lg:gap-6
+                      2xl:grid-cols-3 2xl:gap-8">
         {project1 && (
           <div className="project-card">
             <img src={project1} alt="Project 1" className="project-img" />
             <h3 className="text-primary text-xl font-montserrat">Landing Page Component</h3>
             <p className="project-p">A landing page component built with React.js and TailwindCSS.</p>
             <div className="relative flex flex-col items-center justify-center">
-              <a href="https://social-links-profile15.netlify.app" className="project-a my-1">View Site</a>
+              <a href="https://social-links-profile15.netlify.app" className="project-a" target="_blank" rel="noreferrer">View Site</a>
             </div>
           </div> )
         }
@@ -23,7 +25,7 @@ function Projects() {
             <h3 className="text-primary text-xl font-montserrat">Results Card Component</h3>
             <p className="project-p">A responsive results card component built with React.js and TailwindCSS.</p>
             <div className="relative flex flex-col items-center justify-center">
-              <a href="https://results-summary-component15.netlify.app" className="project-a">View Site</a>
+              <a href="https://results-summary-component15.netlify.app" className="project-a" target="_blank" rel="noreferrer">View Site</a>
             </div>
           </div> )
         }
