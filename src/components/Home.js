dd3d32c7d0beb6d98f6d46eaf76f8a9cbf1d8fa7
marioCoding <marosw.sal@gmail.com>
2024-07-26 02:45:23 -0700

    changed the button tag to an <a> tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,27 +4,27 @@ import Skills from './Skills';
 
 function Home() {
   return (
-    <section id="home" className="relative flex items-center justify-center min-h-screen
-                                  m-6">
+    <section id="home" className="relative flex items-center justify-center min-h-screen">
       <div className="relative flex items-center justify-center min-h-screen flex-col text-center">
         <img 
           src={profilePhoto} 
           alt="Profile" 
-          className="rounded-full m-4 border-8 border-secondary" 
+          className="rounded-full border-8 border-secondary" 
         />
         <h1 className="text-3xl text-primary my-2 font-montserrat font-bold">Mario Salinas</h1>
         <h2 className="font-merriweather text-text mx-4 mb-2">
           Computer Science student and web developer passionate about innovative solutions and enhancing user experience.
         </h2>
         <Skills />
-        <button className="w-1/2 h-2/3 py-2 rounded-2xl
+        <a href="#projects"
+          className="w-1/2 h-2/3 py-2 rounded-2xl
                           bg-accent text-white 
                           border-2 border-accent
                           animate-shadow-pulse font-montserrat
                           hover:bg-secondary hover:text-accent
                           transition-colors duration-500 ease-linear">
           View Projects
-        </button>
+        </a>
       </div>
     </section>
   );
